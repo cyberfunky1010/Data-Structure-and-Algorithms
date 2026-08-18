cdc917791a1ab035e1ce69ed6564cb9406853926
cyberfunky <aquibbinali369@gmail.com>
2026-08-18 21:54:59 +0530

    remove-duplicates-sorted-array

--- 2-pointers/remove-duplicates.py
+++ 2-pointers/remove-duplicates.py
@@ -2,26 +2,26 @@
 
 # [1,1,1,2,2,2,3,3,3,3,4,4,4] - sorted array - return k = 4 & [1,2,3,4]
 
-arr = [1,1,1,2,2,3]
+arr = [1,1,1,2,2,3,3,3,5]
 
 def remove_duplicate(arr):
-     a = 0
-     uniq = 1
-     b = 1
 
-     n = len(arr)
+    if not arr:
+        return 0
+     
+    write_idx = 0   # write pointer
+    read_idx = 1    # read pointer
 
-     while (b < n):
-          if arr[b] == arr[b - 1]:
-               b += 1
+    n = len(arr)
 
-          arr[a + 1] = arr[a]
+    while read_idx < n:
+        if arr[read_idx] != arr[write_idx]:    # checks for unique elements 
+           write_idx += 1
+           arr[write_idx] = arr[read_idx]     # write unique elements
 
-          a += 1
-          b += 1
-          uniq += 1
+        read_idx += 1           # if duplicate just duplicate
 
-     return uniq
+    return write_idx + 1             # length is index + 1
 
 
 print( remove_duplicate(arr))
